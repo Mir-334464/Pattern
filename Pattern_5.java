117931cc4a17a4caa41099f84d8adab10eaeb30a
Mir-334464 <106953464+Mir-334464@users.noreply.github.com>
2022-06-30 16:37:35 +0530

    Add files via upload

---
+++ Pattern_5.java
@@ -0,0 +1,25 @@
+ class Pattern_5
+{
+     
+
+       public static void main( String args[])
+          {
+              int i ,j;       
+
+                for(i=1;i<=5;i++) 
+                   {
+
+                    for(j=5;j>=i;j--)
+                     
+                    System.out.print(j+" ");
+                 
+                     System.out.println();
+             }
+           
+
+
+          }
+
+
+ }
+
